f3c9805c8debf91f33238d82cd5c7d81df219a34
Umriya Afini <afini.umriya@gmail.com>
2022-04-13 11:33:22 +0700

    setup dev tool for client

---
+++ client/.prettierrc.js
@@ -0,0 +1,9 @@
+// Rules: https://prettier.io/docs/en/options.html
+module.exports = {
+    useTabs: false,
+    singleQuote: true,
+    arrowParens: 'avoid',
+    proseWrap: 'preserve',
+    endOfLine: 'auto',
+    trailingComma: 'es5'
+};
